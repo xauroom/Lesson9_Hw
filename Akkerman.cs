1d64846f6b570768434fdad2e1e8b2653f3c1928
xauroom <68505520+xauroom@users.noreply.github.com>
2023-06-18 15:45:43 +0300

    Create Akkerman.cs

---
+++ Akkerman.cs
@@ -0,0 +1,27 @@
+// Задача 68: Напишите программу вычисления
+// функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using System;
+
+public static class Program
+{
+	public static void Main()
+	{
+		int Akkerman(int m, int n)
+		{
+			if (m == 0) return n + 1;
+			else if (n == 0) return Akkerman(m - 1, 1);
+			else return Akkerman(m - 1, Akkerman(m, n - 1));
+		}
+		
+		Console.Clear();
+		System.Console.WriteLine("Введите целое число m ");
+		int m = Convert.ToInt32(Console.ReadLine());
+		System.Console.WriteLine("Введите целое число n ");
+		int n = Convert.ToInt32(Console.ReadLine());
+		System.Console.WriteLine(Akkerman(m, n));
+	}
+}
